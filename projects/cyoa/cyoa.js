f15c2d7a50750fb940f06157513b4ff29bd0b5a3
Cheryl Zuckerman <CZtech@gmail.com>
2018-02-13 16:11:48 -0500

    intial commit for while loops

--- projects/cyoa/cyoa.js
+++ projects/cyoa/cyoa.js
@@ -1,12 +1,15 @@
 var response = prompt("Welcome to the Red Hat Development Company! You walked in the door to your new job. You don't see anyone around. You see a door in front of you, and a paper on the floor. Type 'door' to go through the next door, or type 'paper' to pick up the paper from the floor.")
 
-for (var i=0; i<50; i++);
+for (var i=0; i<50; i++) {
 
-switch (response)
-{
+  if(response===null || response = ""){
+    break;
+  }
+
+switch (response) {
 case 'door':
     response = prompt("The door is locked, there is a code panel next to the door. What do you do now? Maybe pick up the paper?");
-  break;
+break;
 
 case 'paper':
     response = prompt("The paper has a number on it. Hmmm, could that be the code to open the door? Try it. Type the code that's on the paper.")
@@ -17,7 +20,24 @@ case null:
 break;
 
 case '07734':
-  prompt("Congratulations, the door opened. You passed your first test!")
+  prompt("Congratulations, the door opened. You passed your first test! Now you opened the door, you see a chair and a window. Do you want to look out the window? Type 'window' or 'sit' to sit in the chair.")
 break;
 
+case 'sit':
+  prompt("You're sitting in the chair, and see a key on the floor. To pick up the key, type 'key'.")
+break;
+
+case 'window':
+  prompt("Looking out the window you see a tree. Do you want to go back outside to the tree? Type 'outside' to go to the tree.")
+break;
+
+
+case null:
+  prompt("Please type in an answer to the last question.")
+
+
+/* close switch loop */
+}
+
+/* close for loop */
 }
