49d9c822b1173e8860bf7a05947aaafad85f51b0
Cheryl Zuckerman <CZtech@gmail.com>
2018-02-13 16:32:53 -0500

    incremental fix for while loops

--- media/while/while.js
+++ media/while/while.js
@@ -1,14 +1,15 @@
 /* Exercise 1: Log numbers 1-10 */
-output.innterHTML "<h2>List numbers 1-10</h2>"
+var output = document.querySelector("#exercise1");
+ output.innterHTML "<h2>List numbers 1-10</h2>"
 var n = 1;
-
 while (n <= 10) {
   output.innerHTML(n);
   n++;
 }
-break;
 
-output.interHTML "<h2>List all even numbers 1-20</h2>"
+
+ var output = document.querySelector("#exercise1");
+ output.interHTML "<h2>List all even numbers 1-20</h2>"
 var n = 2;
 
 while (n <= 20) {
